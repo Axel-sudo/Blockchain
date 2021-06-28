b229725769f43b53add61c8d1873b1f92d16a232
Axel <a_lanyan@techandcodefactory.fr>
2021-06-29 00:55:41 +0200

    Proof of work implementation

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const SHA256 = require('crypto-js/sha256');
 
-
+//Definition d'un bloc
 class Block{
      constructor(index, timestamp, data, previousHash = ''){
          this.index = index;
@@ -8,17 +8,27 @@ class Block{
          this.data = data;
          this.previousHash = previousHash;
          this.hash = this.calculateHash();
+         this.nonce = 0;
      }
 
      calculateHash(){
-         return SHA256(this.index + this.previousHash +this.timestamp + JSON.stringify(this.data)).toString();
+         return SHA256(this.index + this.previousHash +this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
 
      }
- }
 
+     mineBlock(difficulty){
+         while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+             this.nonce++;
+             this.hash = this.calculateHash();
+         }
+         console.log("Block mined" +this.hash);
+     }
+ }
+//Fonctionnement de la chaîne de blocs
  class Blockchain{
      constructor(){
          this.chain = [this.createGenesisBlock()];
+         this.difficulty = 4;
 
      }
 
@@ -32,7 +42,7 @@ class Block{
 
    addBlock(newBlock){
        newBlock.previousHash = this.getLatestBlock().hash;
-       newBlock.hash = newBlock.calculateHash();
+       newBlock.mineBlock(this.difficulty);
        this.chain.push(newBlock);
    }
 
@@ -63,10 +73,11 @@ class Block{
  
  
  let blockChain = new Blockchain();
+
+ console.log('Mining block1...');
  blockChain.addBlock(new Block(1, "28/06/2021", { amount: 4}));
+
+ console.log('Mining block2...');
  blockChain.addBlock(new Block(2, "29/06/2021", { amount: 10}));
  
- console.log('Is blockchain valid?' + blockChain.isChainValid());
  
- 
- //console.log(JSON.stringify(blockChain, null, 4));
